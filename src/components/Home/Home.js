e6a2a8260bc2d3cab68c458e9820fd48083b967b
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-08-07 19:41:58 +0200

    ifnjbvj

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,114 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 import Slider from '../Slider/Slider';
-import OpisProizvoda from '../OpisProizvoda/OpisProizvoda';
+import Onama from './home-about-us.png';
+import Marker from './hot-spot.png';
+import Hotspot from './hotspot-1.png';
+
+
 
 
 class Home extends Component {
+
     render() {
         return (
          <div>
                 <Slider />
-                <OpisProizvoda />
-                <section className="home-category-links">
-                    <div container-fluid>
-                        <img width={900} height={500} src="../../../assets/img/namestaj1.jpg" alt="namestaj" />
-                    </div>
-                    <div>
-                        a b c
-                </div>
-                </section>
+
+                <Grid>
+                    <Row>
+                        <Col>
+                        <div className="home-categories-mainheading">
+                            <h2>Proizvodi</h2>
+                            <div className="home-categories-heading--linija linija"></div>
+                            <p className="paragraf-opisproizvoda">Iako mlad ogranak firme, Pan Bau Max Keramika ima kadar sa višegodišnjim iskustvom u oblasti keramike
+                                i sanitarne opreme. Sa stalnim praćenjem trendova i inovacija u pomenutoj oblasti i saradnji sa kolegama
+                                ogranka izgradnje daje dobru kombijaciju što tržištu donosi svežinu u vidu ponude i usluge.
+                            </p>
+                        </div>
+                   
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        <div className="home-categories-plocice">
+                            <div className="home-categories__linkbox">
+                              <h1 className="home-categories-plocice--heading">Plocice</h1>
+                                    <ul className="home-categories__linkbox--listL">
+                                        <li><a href="#">Podne</a></li>
+                                        <li><a href="#">Beton</a></li>
+                                        <li><a href="#">Drvo</a></li>
+                                        <li><a href="#">Kamen</a></li>
+                                        <li><a href="#">Mermer</a></li>
+                                        <li><a href="#">Mozaik</a></li>
+                                    </ul>
+                                    <ul className="home-categories__linkbox--listR">
+                                        <li><a href="#">Zidne</a></li>
+                                        <li><a href="#">Mat</a></li>
+                                        <li><a href="#">Sjaj</a></li>
+                                        <li><a href="#">Dekor</a></li>
+                                        <li><a href="#">Mozaik</a></li>
+                                    </ul>
+                               
+                            </div>
+                        </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={4}>
+                        <div className="home-categories--sanitarija">
+                        <div className="home-categories__linkbox">
+                           <h1 className="home-categories--sanitarija--heading">Sanitarija</h1>
+                           <ul className="home-categories__linkbox--sanitarijaList">
+                                        <li><a href="#">Lavaboi</a></li>
+                                        <li><a href="#">Wc solje</a></li>
+                                        <li><a href="#">Kade</a></li>
+                                        <li><a href="#">Tus kabine</a></li>
+                                        <li><a href="#">Kupatilski namestaj i ogledala</a></li>
+                                       
+                                    </ul>
+                           </div>
+                        </div>
+                          
+                        </Col>
+                        <Col lg={4}>
+                        <div className="home-categories--slavine">
+                        <div className="home-categories__linkbox">
+                           <h1 className="home-categories--slavine--slavine__heading">Slavine</h1>
+                           </div>
+                        </div>
+                          
+                        </Col>
+                        <Col lg={4}>
+                        <div className="home-categories--ostalo">                  
+                        <div className="home-categories__linkbox">
+                           <h1 className="home-categories--ostalo--heading">Ostalo</h1>
+                           </div>
+                        </div> 
+                        </Col>
+                    </Row>                  
+               </Grid>
              
                 <section className="home-hot-spots">
-                   <img src="../../assets/img/panbaumax_home_v1.jpg" alt="hot spot" className="hot-spot-image"/>
+                   <img src="../../assets/img/panbaumax_home_v1.jpg" alt="hot spot" className="home-hot-spots__image"/>
+                    <div className="home-hot-spots__box1">
+                      <img className="home-hot-spots__marker1" src={Marker} alt={'hot-spot-1'}/> 
+                     </div>
+                                  
+                             <img className="home-hot-spots__product1" src={Hotspot} alt={'hot-spot-11'}/>
+                   
+                    
+                    <div className="home-hot-spots__box2">
+                             <img className="home-hot-spots__marker2" src={Marker} alt={'hot-spot-1'}/>  
+                             <img  src={Hotspot} alt={'hot-spot-11'}/>
+                    </div>
+                    <div className="home-hot-spots__box3">
+                             <img className="home-hot-spots__marker3" src={Marker} alt={'hot-spot-1'}/>  
+                             <img  src={Hotspot} alt={'hot-spot-11'}/>
+                    </div>
+                  
                 </section>
+
                 <Grid >
                 <Row className="row-onama">
                     <Col sm={6} >
@@ -31,10 +118,15 @@ class Home extends Component {
                       <Button className="button-detaljnije">DETALJNIJE</Button>
                     </Col>
                     <Col sm={6}>
-                      <h1> O nama</h1>
+                    <div>
+                        <img src={Onama} alt={'onama'}/>
+                    </div>
                     </Col>
-                </Row>
+                </Row>   
             </Grid>
+            <div className="brand__slider">
+                        gr
+                     </div>
                 </div>
         );
     }
--- src/components/Onama/Onama.js
+++ src/components/Onama/Onama.js
@@ -1,73 +1,107 @@
 import React, { Component } from 'react';
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col, Grid} from 'react-bootstrap';
 import logo from './logo-gradjevina-white.png';
 import namestaj1 from './namestaj1.jpg';
 import namestaj2 from './namestaj2.png';
-import namestaj3 from './namestaj3.png';
-import namestaj4 from './namestaj4.png';
 import namestaj5 from './namestaj5.png';
 import namestaj6 from './namestaj6.png';
 import namestaj7 from './namestaj7.png';
+import namestaj8 from './namestaj8.png';
+import sijalica from './about-us-widget-1-icon.png';
+import sertifikat from './about-us-widget-2-icon.png';
+import kamionce from './about-us-widget-3-icon.png';
 
 class Onama extends Component {
 
-    state = {
-        klasa:''
-    }
 
-    HoverHandler=()=>{
-        this.setState({klasa:'borred'});
-    }
-
-    HoverOf=()=>{
-        this.setState({klasa:''});
-    }
 
     render(){
         return (
             <div className="onama">
-              <div className="header">
-                 <h3>O nama</h3>
+              <section className="header">
+                 <h3 className="header__naslov">O nama</h3>
                  <div className="linija"></div>
-                  <p>Pan Bau Max Keramika je deo firme Pan Bau Max doo od 2016. godine sa sedištem u Pančevu. 
+                  <p  className="header__paragraph">Pan Bau Max Keramika je deo firme Pan Bau Max doo od 2016. godine sa sedištem u Pančevu. 
                       Pošto je Pan Bau Max gradjevinsko preduzeće, logičan sled okolnosti je da se širi u pravcu
                        prvobitne delatnosti, pa se donosi odluka da se otvori salon keramike i sanitarne opreme
                         i time zaokruži jedan deo gradnje.</p>
-                    <p>Iako mlad ogranak firme, Pan Bau Max Keramika ima kadar sa višegodišnjim iskustvom u oblasti
+                    <p className="header__paragraph">Iako mlad ogranak firme, Pan Bau Max Keramika ima kadar sa višegodišnjim iskustvom u oblasti
                          keramike i sanitarne opreme. Sa stalnim praćenjem trendova i inovacija u pomenutoj oblasti i 
                          saradnji sa kolegama iz ogranka izgradnje daje dobru kombijaciju što tržištu donosi svežinu 
                          u vidu ponude i usluge.</p>
                     <p>Posetite nas sajt:</p>
-                    <img src={logo} alt={"logo firme"}/>
-              </div>
-              <section container className="ikonice" onMouseEnter={this.HoverHandler} onMouseLeave={this.HoverOf}>
+                    <img className="header__logo" src={logo} alt={"logo firme"}/>
+              </section>
+
+              <section container className="ikonice" >
                 <Row>
-                    <Col lg={4} className="ik1">
-                    <i class="far fa-lightbulb fa-10x"></i>
-                       <h3>Idejna resenja</h3>
-                       <p>Ukoliko ste neodlučni, mi ćemo Vam ponuditi idejno rešenje Vašeg prostora.</p>
+                    <Col lg={4} >
+                    <div className="ikonice__ikona">
+                        <div className="ikonice__ikona--img">
+                             <img  src={sijalica} alt={"sijalica"}/>
+                        </div>
+                            <div className="ikonice__ikona--description">
+                                <h3 className="ikonice__ikona--description--header">Idejna resenja</h3>
+                                <p className="ikonice__ikona--description--text">Ukoliko ste neodlučni, mi ćemo Vam ponuditi idejno rešenje Vašeg prostora.</p>
+                            </div>
+                      
+                    </div>
+                   
                     </Col> 
-                    <Col lg={4} className="ik2">
-                    <i class="fas fa-award fa-10x"></i>
-                       <h3>Zadovoljnstvo i kvalitet</h3>
-                       <p>Vaše zadovoljstvo nam je podsticaj za dalji rad, pa nastojimo da bude na visokom nivou.</p>
+                    <Col lg={4} >
+                    <div className="ikonice__ikona">
+                        <div className="ikonice__ikona--img">
+                             <img  src={sertifikat} alt={"sertifikat"}/>
+                        </div>
+                            <div className="ikonice__ikona--description">
+                                <h3 className="ikonice__ikona--description--header">Idejna resenja</h3>
+                                <p className="ikonice__ikona--description--text">Ukoliko ste neodlučni, mi ćemo Vam ponuditi idejno rešenje Vašeg prostora.</p>
+                            </div>
+                      
+                    </div>
                     </Col> 
-                    <Col lg={4} className="ik3">
-                    <i class="fas fa-truck fa-8x"></i>
-                       <h3>Prevoz robe</h3>
-                       <p>Besplatno prevozimo robu do Vaše adrese.</p>
+                    <Col lg={4} >
+                    <div className="ikonice__ikona">
+                        <div className="ikonice__ikona--img">
+                             <img  src={kamionce} alt={"kamionce"}/>
+                        </div>
+                            <div className="ikonice__ikona--description">
+                                <h3 className="ikonice__ikona--description--header">Idejna resenja</h3>
+                                <p className="ikonice__ikona--description--text">Ukoliko ste neodlučni, mi ćemo Vam ponuditi idejno rešenje Vašeg prostora.</p>
+                            </div>
+                      
+                    </div>
                     </Col> 
                 </Row>
                </section>
               
                <section className="galerija">
-                 <img  className={this.state.klasa}  src={namestaj1} alt={"galerija1"} />
-                 <img src={namestaj2} alt={"galerija2"} />
+
+               <Row>
+                    <Col lg={3} md={4} sm={6}>
+                       <div className="galerija__box">
+                        <img className="galerija__box__img" src={namestaj2} alt={"galerija2"} />
+                        </div>
+                    </Col>
+
+                   <Col lg={3} md={4} sm={6}>
+                     <div className="galerija__box"> <img className="galerija__box__img" src={namestaj1} alt={"galerija1"} /></div>
+                     <div className="galerija__box"> <img className="galerija__box__img" src={namestaj5} alt={"galerija2"} /></div>
+                   </Col>
+                  
+                   <Col lg={3} md={4}>
+                     <div className="galerija__box"> 
+                       <img className="galerija__box__img" src={namestaj8} alt={"galerija8"} />
+                     </div>
+                   </Col>
+                   <Col lg={3}>
+                     <div className="galerija__box"> <img className="galerija__box__img" src={namestaj6} alt={"galerija6"} /></div>
+                     <div className="galerija__box"> <img className="galerija__box__img" src={namestaj7} alt={"galerija7"} /></div>
+                   </Col>
+               </Row>
+             
                 
-                 <img src={namestaj4} alt={"galerija4"} />
-                 <img src={namestaj5} alt={"galerija5"} />
-                 <img src={namestaj6} className="revealOnScroll" alt={"galerija6"} />
-                 <img className="gal7" src={namestaj7} alt={"galerija7"} />
+              
                </section>     
             </div>
         );
