a428d68f3f7fb939deb8a82b695c16674ed39bea
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-08-17 19:38:39 +0200

    iz huba

--- src/components/Proizvodi/Proizvodi.js
+++ src/components/Proizvodi/Proizvodi.js
@@ -2,30 +2,88 @@ import React, { Component } from 'react';
 import { Grid, Row, Col, Checkbox, Button,FormGroup } from 'react-bootstrap';
 import gal1 from './img-gallery/dark-1.jpg';
 import gal2 from './img-gallery/darkPaint.jpg';
+import axios from 'axios';
 
 class Proizvodi extends Component {
 
   state = {
-    plociceChecked: false,
-    podneChecked: false,
-    zidneChecked: false
+  
+    proizvodi: [],
+    categories:[],
+    proizvodiSelected:[],
+    categoriesSelected:[''],
+    categoriesArray:[]
   }
 
-  handleChange = (event) => {
-    console.log(event.target.checked);
-    this.setState({ plociceChecked: !this.state.plociceChecked});
+  componentDidMount() {
+    axios.get(`https://panbaumax.000webhostapp.com/wp-json/wp/v2/proizvodi/?per_page=100`)
+      .then(res => {
+        const proizvodi = res.data;
+        this.setState({ proizvodi });
+        console.log(proizvodi); 
+      })
+
+      axios.get(`https://panbaumax.000webhostapp.com/wp-json/wp/v2/categories/?per_page=50`)
+      .then(res => {
+        const categories = res.data;
+        this.setState({ categories });
+        console.log(categories); 
+
+        var allCategories=[];
+
+        for(var prop in categories){
+          var category = {
+                          id:categories[prop].id,
+                          name:categories[prop].name,
+                          parent:[categories[prop].parent],
+                          selected: false 
+                        }
+                          for(var i in categories){
+                            if(category.parent[0]===categories[i].id){
+                              if(categories[i].parent !==0 && categories[i].parent !==6){
+                                category.parent.push(categories[i].parent)
+                              }
+                            }
+                          }
+          allCategories.push(category);
+        }
+      
+        console.log(allCategories)
+      })
+   
   }
 
-  handleIsItCheckedZidneorPodne= () => {
-    console.log(this.state.plociceChecked ? 'Yes' : 'No');
+  filterCat=(naziv,index)=>{
+    let catIndexi= [...this.state.categoriesArray,index];
+    let i=0;
+    let proizvodiSelected =[];
+
+    for(var prop in catIndexi){
+       let sortIndex= catIndexi[prop];
+       console.log(sortIndex);
+      
+       proizvodiSelected = this.state.proizvodi.filter(kategorija => kategorija.categories[i] === catIndexi[0]);
+    }
+   
+    
+    let catNaziv = [...this.state.categoriesSelected, naziv];
+    this.setState({proizvodiSelected, categoriesSelected:catNaziv, categoriesArray:catIndexi});
+    console.log(proizvodiSelected);
+    console.log(this.state.categoriesSelected);
+    console.log(catIndexi);
   }
 
-  handleToggle = () => {
-    this.state.plociceChecked = !this.state.plociceChecked;
-    this.setState({ plociceChecked: this.state.plociceChecked });
-  }
+  // filterBeton=(naziv,index)=>{
+  //   let proizvodiSelected = this.state.proizvodi.filter(kategorija => kategorija.categories[0] === index);
+  //   let catNazivBeton =[...this.state.categoriesSelected, naziv];
+  //   this.setState({proizvodiSelected, categoriesSelected:catNazivBeton});
+  //   console.log(proizvodiSelected);
+  //   console.log(this.state.categoriesSelected);
+  // }
+  
 
-  render() {
+  render() { 
+  
     return (
       <section className="proizvodi">
         <div className="proizvodi-header">
@@ -35,72 +93,28 @@ class Proizvodi extends Component {
             <div className="proizvodi__container__filter">
               <h2 className="h2--filter">Filteri</h2> <div className="proizvodi linija"></div>
               <div className="proizvodi__container__filter--1">
-                <ul className="proizvodi__container__filter--1--ul"><span>Plocice</span><svg className="icon-strelica">
+
+               <p onClick={()=>this.filterCat('Drvo',13)}>DRVO</p>
+                <p onClick={()=>this.filterCat('Beton',35)}>BETON</p>
+                  <ul>
+                    {
+                   this.state.categoriesSelected.map(cat=>{
+                     return <li key={cat}>{cat}</li>
+                   })
+                 }
+                </ul>
+                <ul className="proizvodi__container__filter--1--ul"><svg className="icon-strelica">
                   <use xlinkHref="sprite.svg#icon-chevron-thin-down"></use>
                 </svg>
-                  <form  >
-                    <FormGroup bsSize="large">
-                      <Checkbox
-                        onChange={this.handleChange}
-                        checked={this.state.plociceChecked}>Plocice</Checkbox>
-                      {/* <Button type="button" onClick={this.handleToggle}>Toggle</Button>
-            <Button type="button" onClick={this.handleIsItChecked}>Is it checked?</Button> */}
-                      <Checkbox
-                        onChange={this.handleChange}
-                        checked={this.state.podneChecked}>Podne</Checkbox>
-                      <Checkbox
-                        onChange={this.handleChange}
-                        checked={this.state.zidneChecked}>Zidne</Checkbox>
-                    </FormGroup>
-                  </form>
-
-                  <ul className="proizvodi__container__filter--1--ul"><span>Podne</span>
-                    <li>Beton</li>
-                    <li>Drvo</li>
-                    <li>Kamen</li>
-                    <li>Mermer</li>
-                    <li>Mozaik</li>
-                  </ul>
-
-                  <ul className="proizvodi__container__filter--1--ul"><span>Zidne</span>
-                    <li>Dekor</li>
-                    <li>Mat</li>
-                    <li>Mozaik</li>
-                    <li>Sjaj</li>
-                  </ul>
-                </ul>
-              </div>
-
-              <div className="proizvodi__container__filter--2">
-                <ul className="proizvodi__container__filter--2--ul">
-                  <li>adddddddddddddddddd</li>
-                  <li>s</li>
-                  <li>d</li>
-                  <li>f</li>
-                </ul>
-              </div>
-
-              <div className="proizvodi__container__filter--3">
-                <ul className="proizvodi__container__filter--3--ul">
-                  <li>adddddddddddddddddd</li>
-                  <li>s</li>
-                  <li>d</li>
-                  <li>f</li>
-                </ul>
-              </div>
-
-              <div className="proizvodi__container__filter--4">
-                <ul className="proizvodi__container__filter--4--ul">
-                  <li>adddddddddddddddddd</li>
-                  <li>s</li>
-                  <li>d</li>
-                  <li>f</li>
+               
+                 {
+                   this.state.categories.map(cat=>{
+                     return <li key={cat.id}>{cat.id}--{cat.name}</li>
+                   })
+                 }
                 </ul>
               </div>
             </div>
-
-
-
           </Col>
 
           <Col lg={5}>
@@ -153,6 +167,25 @@ class Proizvodi extends Component {
           </Col>
 
           <Col lg={4}>
+          <ul>
+        {this.state.proizvodiSelected.map(proizvod =>
+        <li key={proizvod.id}>
+          {proizvod.title.rendered}-//-
+          {proizvod.categories}</li>
+        
+       )}
+      </ul>
+          -------------------------------------
+          <ul>
+        {this.state.proizvodi.map(proizvod =>
+        <li key={proizvod.id}>
+          {proizvod.title.rendered}-//-
+          {proizvod.categories}</li>  
+       )}
+      </ul>
+      ------------------------
+    
+     
           </Col>
         </Row>
       </section>
